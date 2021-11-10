7d9192013ebd5b34bbce0a9617a8800323551ade
KIKZZY <kikzzy-r2@hotmail.com>
2021-11-09 19:38:43 -0500

    Se añaden alertas

--- AsistenciaMicroing/app/src/main/java/com/kmac/asistenciamicroing/LoginActivity.java
+++ AsistenciaMicroing/app/src/main/java/com/kmac/asistenciamicroing/LoginActivity.java
@@ -3,15 +3,18 @@ package com.kmac.asistenciamicroing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.view.View;
 import android.content.Intent;
 
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText etName;
+    private EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +23,22 @@ public class LoginActivity extends AppCompatActivity {
     }
     public void goToMain(View view){
         etName = findViewById(R.id.etName);
+        etPassword = findViewById(R.id.etPassword);
+
+        Resources res = getResources();
+
         String name =etName.getText().toString();
+        String pass =etPassword.getText().toString();
 
-        Intent intentMain = new Intent(this, MainActivity.class);
+        if (!name.equals("")&& !pass.equals((""))) {
+            Intent intentMain = new Intent(this, MainActivity.class);
 
-        intentMain.putExtra("user", name);
+            intentMain.putExtra("user", name);
 
-        startActivity(intentMain);
+            startActivity(intentMain);
+        }else{
+            Toast.makeText(this, res.getString(R.string.Campos_Vacios), Toast.LENGTH_LONG).show();
+        }
+
     }
 }
--- AsistenciaMicroing/app/src/main/java/com/kmac/asistenciamicroing/RColaborador_Activity.java
+++ AsistenciaMicroing/app/src/main/java/com/kmac/asistenciamicroing/RColaborador_Activity.java
@@ -3,12 +3,24 @@ package com.kmac.asistenciamicroing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class RColaborador_Activity extends AppCompatActivity {
+
+    private Spinner spinner1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rcolaborador);
+
+        spinner1= (Spinner) findViewById(R.id.snCargo);
+
+        String [] cargo= {"Supervisor", "Oficial", "Ayudante Práctico", "Ayudante", "Subcontratista"};
+
+        ArrayAdapter <String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cargo);
+        spinner1.setAdapter(adapter);
     }
 }
